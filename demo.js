6ee0e10427fdd0b6247681991dabad094913032f
Pradnya11s <115912859+Pradnya11s@users.noreply.github.com>
2022-10-17 08:28:26 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -4,3 +4,9 @@ Header.textContent = "This is Title" ;
 
 
 header.style.borderBottom = 'solid 3px #000';
+
+var items = document .getElementbyClassName('list-group-item');
+
+items[2].style.backgroundColor = 'green';
+
+items[2].style.fontWeight = 'bold';
